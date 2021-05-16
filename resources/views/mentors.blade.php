148d2d7aba1dba12b3fa229f0d004ff834f8cb4f
mohdmustfa <mdmustfamalik786@gmail.com>
2021-05-16 21:55:01 +0530

    add: mentors page structure and style

---
+++ resources/views/mentors.blade.php
@@ -0,0 +1,280 @@
+@extends('layouts.app')
+@section('content')
+<section class="mentors pt-5">
+  <div class="container">
+  
+<!-- mentors grid -->
+   <div class="row">
+      <div class="col-12">
+         <div class="card p-3">
+           <div class="row">
+               <div class="col-lg-3 col-md-6 col-12">
+                  <div class="card-mentors">
+                     <div class="upper-box">
+                       <div class="cover-bg">
+                          <img src="{{ asset('Images/cover-mentors.jpg') }}" alt="cover-bg" class="img-fluid">
+                       </div>
+                       <div class="profile-box">
+                        <a href="#about-mentor"  data-toggle="modal" class="profile-popup">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                        </a>
+                       </div>
+                       <div class="chat-icon">
+                          <a href="#" title="Leave message">
+                          <span><i class="fas fa-comment-alt"></i></span>
+                          </a>
+                       </div>
+                     </div>
+
+                     <div class="body-content">
+                        <h4>Josh butler peter</h4>
+                        <p><span>Gym trainer</span> <span>dehradun city</span> </p>
+                        <h6><span><i class="fas fa-atom"></i></span> 6 years of experience</h6>
+                        <a href="#" class="btn btn-small">Connect</a>
+                     </div>
+                  </div>
+               </div>
+               <!-- /column -->
+
+               <div class="col-lg-3 col-md-6 col-12">
+                  <div class="card-mentors">
+                     <div class="upper-box">
+                       <div class="cover-bg">
+                          <img src="{{ asset('Images/cover-mentors.jpg') }}" alt="cover-bg" class="img-fluid">
+                       </div>
+                       <div class="profile-box">
+                        <a href="#about-mentor"  data-toggle="modal" class="profile-popup">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                        </a>
+                       </div>
+                       <div class="chat-icon">
+                          <a href="#" title="Leave message">
+                          <span><i class="fas fa-comment-alt"></i></span>
+                          </a>
+                       </div>
+                     </div>
+
+                     <div class="body-content">
+                        <h4>Josh butler peter</h4>
+                        <p><span>Gym trainer</span> <span>dehradun city</span> </p>
+                        <h6><span><i class="fas fa-atom"></i></span> 6 years of experience</h6>
+                        <a href="#" class="btn btn-small">Connect</a>
+                     </div>
+                  </div>
+               </div>
+               <!-- /column -->
+
+               <div class="col-lg-3 col-md-6 col-12">
+                  <div class="card-mentors">
+                     <div class="upper-box">
+                       <div class="cover-bg">
+                          <img src="{{ asset('Images/cover-mentors.jpg') }}" alt="cover-bg" class="img-fluid">
+                       </div>
+                       <div class="profile-box">
+                        <a href="#about-mentor"  data-toggle="modal" class="profile-popup">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                        </a>
+                       </div>
+                       <div class="chat-icon">
+                          <a href="#" title="Leave message">
+                          <span><i class="fas fa-comment-alt"></i></span>
+                          </a>
+                       </div>
+                     </div>
+
+                     <div class="body-content">
+                        <h4>Josh butler peter</h4>
+                        <p><span>Gym trainer</span> <span>dehradun city</span> </p>
+                        <h6><span><i class="fas fa-atom"></i></span> 6 years of experience</h6>
+                        <a href="#" class="btn btn-small">Connect</a>
+                     </div>
+                  </div>
+               </div>
+               <!-- /column -->
+
+               <div class="col-lg-3 col-md-6 col-12">
+                  <div class="card-mentors">
+                     <div class="upper-box">
+                       <div class="cover-bg">
+                          <img src="{{ asset('Images/cover-mentors.jpg') }}" alt="cover-bg" class="img-fluid">
+                       </div>
+                       <div class="profile-box">
+                        <a class="profile-popup" href="#about-mentor"  data-toggle="modal">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                        </a>
+                       </div>
+                       <div class="chat-icon">
+                          <a href="#" title="Leave message">
+                          <span><i class="fas fa-comment-alt"></i></span>
+                          </a>
+                       </div>
+                     </div>
+
+                     <div class="body-content">
+                        <h4>Josh butler peter</h4>
+                        <p><span>Gym trainer</span> <span>dehradun city</span> </p>
+                        <h6><span><i class="fas fa-atom"></i></span> 6 years of experience</h6>
+                        <a href="#" class="btn btn-small">Connect</a>
+                     </div>
+                  </div>
+               </div>
+               <!-- /column -->
+               <div class="col-lg-3 col-md-6 col-12">
+                  <div class="card-mentors">
+                     <div class="upper-box">
+                       <div class="cover-bg">
+                          <img src="{{ asset('Images/cover-mentors.jpg') }}" alt="cover-bg" class="img-fluid">
+                       </div>
+                       <div class="profile-box">
+                        <a class="profile-popup" href="#about-mentor"  data-toggle="modal">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                        </a>
+                       </div>
+                       <div class="chat-icon">
+                          <a href="#" title="Leave message">
+                          <span><i class="fas fa-comment-alt"></i></span>
+                          </a>
+                       </div>
+                     </div>
+
+                     <div class="body-content">
+                        <h4>Josh butler peter</h4>
+                        <p><span>Gym trainer</span> <span>dehradun city</span> </p>
+                        <h6><span><i class="fas fa-atom"></i></span> 6 years of experience</h6>
+                        <a href="#" class="btn btn-small">Connect</a>
+                     </div>
+                  </div>
+               </div>
+               <!-- /column -->
+               <div class="col-lg-3 col-md-6 col-12">
+                  <div class="card-mentors">
+                     <div class="upper-box">
+                       <div class="cover-bg">
+                          <img src="{{ asset('Images/cover-mentors.jpg') }}" alt="cover-bg" class="img-fluid">
+                       </div>
+                       <div class="profile-box">
+                        <a class="profile-popup" href="#about-mentor"  data-toggle="modal">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                        </a>
+                       </div>
+                       <div class="chat-icon">
+                          <a href="#" title="Leave message">
+                          <span><i class="fas fa-comment-alt"></i></span>
+                          </a>
+                       </div>
+                     </div>
+
+                     <div class="body-content">
+                        <h4>Josh butler peter</h4>
+                        <p><span>Gym trainer</span> <span>dehradun city</span> </p>
+                        <h6><span><i class="fas fa-atom"></i></span> 6 years of experience</h6>
+                        <a href="#" class="btn btn-small">Connect</a>
+                     </div>
+                  </div>
+               </div>
+               <!-- /column -->
+               <div class="col-lg-3 col-md-6 col-12">
+                  <div class="card-mentors">
+                     <div class="upper-box">
+                       <div class="cover-bg">
+                          <img src="{{ asset('Images/cover-mentors.jpg') }}" alt="cover-bg" class="img-fluid">
+                       </div>
+                       <div class="profile-box">
+                        <a class="profile-popup" href="#about-mentor"  data-toggle="modal">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                        </a>
+                       </div>
+                       <div class="chat-icon">
+                          <a href="#" title="Leave message">
+                          <span><i class="fas fa-comment-alt"></i></span>
+                          </a>
+                       </div>
+                     </div>
+
+                     <div class="body-content">
+                        <h4>Josh butler peter</h4>
+                        <p><span>Gym trainer</span> <span>dehradun city</span> </p>
+                        <h6><span><i class="fas fa-atom"></i></span> 6 years of experience</h6>
+                        <a href="#" class="btn btn-small">Connect</a>
+                     </div>
+                  </div>
+               </div>
+               <!-- /column -->
+               <div class="col-lg-3 col-md-6 col-12">
+                  <div class="card-mentors">
+                     <div class="upper-box">
+                       <div class="cover-bg">
+                          <img src="{{ asset('Images/cover-mentors.jpg') }}" alt="cover-bg" class="img-fluid">
+                       </div>
+                       <div class="profile-box">
+                        <a class="profile-popup" href="#about-mentor"  data-toggle="modal">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                        </a>
+                       </div>
+                       <div class="chat-icon">
+                          <a href="#" title="Leave message">
+                          <span><i class="fas fa-comment-alt"></i></span>
+                          </a>
+                       </div>
+                     </div>
+
+                     <div class="body-content">
+                        <h4>Josh butler peter</h4>
+                        <p><span>Gym trainer</span> <span>dehradun city</span> </p>
+                        <h6><span><i class="fas fa-atom"></i></span> 6 years of experience</h6>
+                        <a href="#" class="btn btn-small">Connect</a>
+                     </div>
+                  </div>
+               </div>
+               <!-- /column -->
+
+           </div>
+         </div>
+      </div>
+   </div>
+  </div>
+
+  <!-- about popup -->
+<!-- Modal -->
+<div class="modal fade modal-about-mentor" id="about-mentor" tabindex="-1" role="dialog" aria-labelledby="about-mentorlLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      <div class="modal-body p-0">
+         <div class="about-card">
+            <div class="cover-bg">
+                  <img src="{{ asset('Images/cover-mentors.jpg') }}" alt="cover-bg" class="img-fluid">
+            </div>
+         <div class="profile-box">
+            <a class="profile-popup" href="#">
+                 <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+            </a>
+         </div>
+         <div class="about-body">
+            <div class="button-strip">
+                <a href="#" class="btn btn-small">Connect</a>
+                <a href="#" class="btn btn-small">Message</a>
+                <a href="#" class="btn btn-small">More</a>
+            </div>
+            <div class="about-info-box">
+               <h4 class="mb-0">Josh butler peter</h4>
+               <p><span>Gym trainer at Royal Gym</span> </p>
+               <h6>Deharadun, Uttarakhand, india <span class="dot"></span> <span>6 years of experience</span> <span class="dot"></span> <a href="#" class="">Contact Info</a></h6>
+            </div>
+         </div>
+         </div>
+         <hr>
+      <div class="about-info-sec">
+         <h5>About</h5>
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est dolores iste neque, quas, sit at sapiente ullam tenetur reprehenderit ea adipisci, a voluptas consequuntur. Accusamus consequuntur a voluptas eligendi non?</p>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+</section>
+
+@endsection
