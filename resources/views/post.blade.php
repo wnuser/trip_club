267c0ad87a181160433ea47b9efbfafb3509ea08
mohdmustfa <mdmustfamalik786@gmail.com>
2021-05-23 22:51:17 +0530

    feed page styling and add some structure

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
    <div class="container">
         <div class="row">
             <div class="col-lg-3 col-md-12 col-12">
-                 <div class="card side-card p-3">
+                 <div class="card side-card py-3 sticky-dektop card-anchors">
                      <a href="#">
                       connetions
                       <span>growth your network</span>
@@ -24,8 +24,8 @@
             <div class="col-lg-6 col-md-12 col-12">
                <div class="card post-card p-3">
                    <div class="d-flex">
-                    <div class="profile-img">
-                    <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                     <div class="profile-img">
+                     <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
                     </div>
                     <a href="#">
                       Write a post
@@ -42,7 +42,9 @@
                 <div class="card feed-card p-3">
                    <div class="d-flex">
                        <div class="profile-img">
+                          <a href="#">
                           <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                          </a>
                         </div>
                         <div class="name-post">
                            <h6>John Doe</h6>
@@ -56,11 +58,37 @@
                       </div>
                    </div>
                 </div>
+                <!-- /card feed -->
+                <div class="card feed-card p-3">
+                   <div class="d-flex">
+                       <div class="profile-img">
+                          <a href="#">
+                          <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                          </a>
+                        </div>
+                        <div class="name-post">
+                           <h6>John Doe</h6>
+                           <span>Gym trainer dehradun</span>
+                        </div>
+                   </div>
+                   <div class="post-content">
+                      <p>I am looking for a job, I have arround 3 years experience in php developer. I can work only home.</p>
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti quibusdam nulla? Quidem, quam adipisci nobis consectetur facilis voluptatum, fuga reiciendis officiis beatae ratione nisi eligendi culpa corrupti officia. Earum!.</p>
+                   </div>
+                </div>
 
             </div>
             <div class="col-lg-3 col-md-12 col-12">
-                 <div class="card p-3">
-                  <h5>Latest feed</h5>
+                 <div class="card py-3 sticky-dektop card-anchors">
+                    <div class="filter-sd px-3">
+                       <h5>Sort</h5>
+                       <select name="filter" id="">
+                       <option value="1">Recent</option>
+                          <option value="2">Top</option>
+                       </select>
+                    </div>
+                    <hr>
+                  <h5 class="px-3">Latest feed</h5>
                    <a href="#">
                      <h6>Product-based company or Service based company</h6>
                       <span>Let's Connect on Twitter:</span>
