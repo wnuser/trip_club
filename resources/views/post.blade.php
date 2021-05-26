54b08c6d65392a2a26ac9dd7b68135a5e4b0f85b
mohdmustfa <mdmustfamalik786@gmail.com>
2021-05-26 23:34:20 +0530

    feed page sidebar, create post file and hover popup etc

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -30,17 +30,12 @@
                     <a href="#create-post" data-toggle="modal">
                       Write a post
                     </a>
-                    <div class="photo-upload">
-                        <form action="#">
-                           <label for="upload-img"><i class="fas fa-image"></i> <span>Photo</span> </label>
-                          <input type="file" class="form-control" id="upload-img"/>
-                        </form>
-                    </div>
+                    
                    </div>
                </div>
 
                 <div class="card feed-card p-3">
-                   <div class="d-flex">
+                   <div class="d-flex hover-box-wrap">
                        <div class="profile-img">
                           <a href="#">
                           <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
@@ -49,6 +44,22 @@
                         <div class="name-post">
                            <h6>John Doe</h6>
                            <span>Gym trainer dehradun</span>
+                        </div>
+                        <div class="hover-info">
+                           <div class="name-info">
+                              <div class="profile-img">
+                              <a href="#">
+                              <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                              </a>
+                              </div>
+                              <div class="name-post">
+                                 <h6>John Doe</h6>
+                                 <span>Gym trainer dehradun</span>
+                              </div>
+                           </div>
+                           <div class="chat-btn">
+                               <a href="#" class="btn btn-small">Chat</a>
+                           </div>
                         </div>
                    </div>
                    <div class="post-content">
@@ -57,10 +68,18 @@
                           <img src="{{ asset('Images/cover-mentors.jpg') }}" alt="cover-bg" class="img-fluid">
                       </div>
                    </div>
+                   <div class="action-count">
+                      <span class="count"><span><i class="far fa-heart"></i></span> <span>145</span> </span>
+                      <span class="count"><span><i class="far fa-comments"></i></span> <span>78</span> </span>
+                   </div>
+                   <div class="action-box">
+                      <a href="#"><span><i class="far fa-heart"></i></span> Like</a>
+                      <a href="#"><span><i class="far fa-comments"></i></span> Comment</a>
+                   </div>
                 </div>
                 <!-- /card feed -->
                 <div class="card feed-card p-3">
-                   <div class="d-flex">
+                   <div class="d-flex hover-box-wrap">
                        <div class="profile-img">
                           <a href="#">
                           <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
@@ -70,10 +89,34 @@
                            <h6>John Doe</h6>
                            <span>Gym trainer dehradun</span>
                         </div>
+                        <div class="hover-info">
+                           <div class="name-info">
+                              <div class="profile-img">
+                              <a href="#">
+                              <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                              </a>
+                              </div>
+                              <div class="name-post">
+                                 <h6>John Doe</h6>
+                                 <span>Gym trainer dehradun</span>
+                              </div>
+                           </div>
+                           <div class="chat-btn">
+                               <a href="#" class="btn btn-small">Chat</a>
+                           </div>
+                        </div>
                    </div>
                    <div class="post-content">
                       <p>I am looking for a job, I have arround 3 years experience in php developer. I can work only home.</p>
                       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti quibusdam nulla? Quidem, quam adipisci nobis consectetur facilis voluptatum, fuga reiciendis officiis beatae ratione nisi eligendi culpa corrupti officia. Earum!.</p>
+                   </div>
+                   <div class="action-count">
+                      <span class="count"><span><i class="far fa-heart"></i></span> <span>145</span> </span>
+                      <span class="count"><span><i class="far fa-comments"></i></span> <span>78</span> </span>
+                   </div>
+                   <div class="action-box">
+                      <a href="#"><span><i class="far fa-heart"></i></span> Like </a>
+                      <a href="#"><span><i class="far fa-comments"></i></span> Comment</a>
                    </div>
                 </div>
 
@@ -88,15 +131,41 @@
                        </select>
                     </div>
                     <hr>
+                    <a href="#">
+                   <div class="d-flex sidebar-list">
+                      <div class="profile-img">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                      </div>
+                      <div class="name-post">
+                        <h6>John Doe</h6>
+                        <span>Gym trainer dehradun</span>
+                      </div>
+                   </div>
+                   </a>
+                   <a href="#">
+                   <div class="d-flex sidebar-list">
+                      <div class="profile-img">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                      </div>
+                      <div class="name-post">
+                        <h6>John Doe</h6>
+                        <span>Gym trainer dehradun</span>
+                      </div>
+                   </div>
+                   </a>
+                   <a href="#">
+                   <div class="d-flex sidebar-list">
+                      <div class="profile-img">
+                        <img src="{{ asset('Images/solo.jpg') }}" alt="profile" class="img-fluid">
+                      </div>
+                      <div class="name-post">
+                        <h6>John Doe</h6>
+                        <span>Gym trainer dehradun</span>
+                      </div>
+                   </div>
+                   </a>
+                   <hr>
                   <h5 class="px-3">Latest feed</h5>
-                   <a href="#">
-                     <h6>Product-based company or Service based company</h6>
-                      <span>Let's Connect on Twitter:</span>
-                   </a>
-                   <a href="#">
-                     <h6>Product-based company or Service based company</h6>
-                      <span>Let's Connect on Twitter:</span>
-                   </a>
                    <a href="#">
                      <h6>Product-based company or Service based company</h6>
                       <span>Let's Connect on Twitter:</span>
@@ -141,6 +210,10 @@
                <textarea name="write-post" id="" placeholder="Write something here.." autofocus="autofocus"></textarea>   
          </div>
          <div class="modal-footer text-right pt-1 pb-2">
+              <div class="photo-upload">
+                  <label for="upload-img"><i class="fas fa-image"></i> <span>Photo</span> </label>
+                  <input type="file" class="form-control" id="upload-img"/>
+               </div>
              <a href="#" class="btn btn-small">Save</a>
          </div>
          </form>
