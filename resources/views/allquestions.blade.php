0d3086b404861218c8513a7afbede06bf7867d6c
wnuser <wn11group@gmail.com>
2021-06-05 23:06:38 +0530

    added questions

--- resources/views/allquestions.blade.php
+++ resources/views/allquestions.blade.php
@@ -21,18 +21,24 @@
                             <h6>{{ $value->seekers->name }}</h6>
                         </div>
                         <div class="question">
-                        @php $isAnswered   = false; @endphp
-                        @foreach($value->answers as $kk => $vv)
-                            @if($vv->answerMentor->id  == Auth::user()->id)
-                               @php $isAnswered = true;  @endphp
-                            @endif
-                        @endforeach
+                        @if(Auth::check())
+                            @php $isAnswered   = false; @endphp
+                            @foreach($value->answers as $kk => $vv)
+                                @if($vv->answerMentor->id  == Auth::user()->id)
+                                @php $isAnswered = true;  @endphp
+                                @endif
+                            @endforeach
+                        @endif
+
 
                         <h5>Q. {{ $value->question }} </h5>
-                        @if(Auth::user()->user_type == config('role.ROLES.MENTOR.TYPE') && !$isAnswered)
-                              <button class="btn btn-primary float-right" data-toggle="modal" data-target="#answerModal{{$value->id}}"> <i class="fa fa-edit"></i> Write Answer</button>
+                        @if(Auth::check())
+                            @if(Auth::user()->user_type == config('role.ROLES.MENTOR.TYPE') && !$isAnswered)
+                                <button class="btn btn-primary float-right" data-toggle="modal" data-target="#answerModal{{$value->id}}"> <i class="fa fa-edit"></i> Write Answer</button>
+                            @endif
                         @endif
                         </div>
+
                     </div>
                      <div class="answers-wrapper">
                         <span class="totol-ans">{{ $value->answers->count() }} Answers</span>
@@ -82,6 +88,7 @@
 
                     <!-- Modal body -->
                     <div class="modal-body">
+                    @if(Auth::check())
                         <form action="{{ route('save.answer') }}"   method="POST">
                             @csrf
                             <div class="form-group">
@@ -89,14 +96,15 @@
                             </div>
                             <div class="form-group">
                                 <textarea name="answer" id="" cols="30" rows="10"></textarea>
+
                                 <input type="hidden" value="{{ Auth::user()->id }}" name="mentor_id">
                                 <input type="hidden" value="{{ $value->id }}" name="question_id">
                             </div>
                             <div class="form-group">
                                 <button type="submit" class="btn btn-small float-right">Submit</button>
                             </div>
-                            
                         </form>
+                    @endif    
                     </div>
 
                     <!-- Modal footer -->
@@ -173,9 +181,9 @@
                <div class="card sticky-dektop py-3 related-que">
                    <h5 class="px-3">Related Questions</h5>
                    <hr>
-                   <a href=""#><strong>Gym trainer</strong>  <span>Someting someting someting someting someting</span> </a>
-                   <a href=""#><strong>Doctor</strong> <span>officia voluptatum nostrum repudiandae odit iste illum laborum consequuntur molestiae doloribus</span> </a>
-                   <a href=""#><strong>Yoga Teacher</strong> <span>officia voluptatum nostrum odit iste illum laborum consequuntur molestiae doloribus</span> </a>
+                   <a href=""#> <span>Q.Someting someting someting someting someting</span> </a>
+                   <a href=""#><span>Q.Officia voluptatum nostrum repudiandae odit iste illum laborum consequuntur molestiae doloribus</span> </a>
+                   <a href=""#><span>Q.Officia voluptatum nostrum odit iste illum laborum consequuntur molestiae doloribus</span> </a>
 
                </div>
             </div>
